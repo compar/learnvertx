a7294e9ea83562b1974ad9e7fccf608a7173e463
compar <81157921@qq.com>
2020-09-24 17:07:58 +0800

    添加集群方式的EventBus测试

--- src/main/java/compar/demo/vertxdemo/verticles/EventLauncher2.java
+++ src/main/java/compar/demo/vertxdemo/verticles/EventLauncher2.java
@@ -6,7 +6,7 @@ import compar.demo.vertxdemo.lanucher.SingleLauncher;
 import io.vertx.core.DeploymentOptions;
 
 
-public class EventLauncher {
+public class EventLauncher2 {
     public static void main(final String[] args) {
         // 哪种模式？
         final boolean isClustered = true;
@@ -15,12 +15,12 @@ public class EventLauncher {
         System.out.println(Thread.currentThread().getName() + ","
                 + Thread.currentThread().getId());
         launcher.start(vertx -> {
-            // 发布Standard
-            vertx.deployVerticle("compar.demo.vertxdemo.verticles.AcceptorVerticle",
-                    new DeploymentOptions().setInstances(4));
-//            // 发布Worker
-//            vertx.deployVerticle("compar.demo.vertxdemo.verticles.WorkerVerticle",
-//                    new DeploymentOptions().setWorker(true).setInstances(16));
+//            // 发布Standard
+//            vertx.deployVerticle("compar.demo.vertxdemo.verticles.AcceptorVerticle",
+//                    new DeploymentOptions().setInstances(4));
+            // 发布Worker
+            vertx.deployVerticle("compar.demo.vertxdemo.verticles.WorkerVerticle",
+                    new DeploymentOptions().setWorker(true).setInstances(16));
         });
     }
 }
